f0d0039d69fdfc23edd3b6ea2ae313f0f66d000b
Lucy Moore & Suresh Swamy <pair+lucymoore.sureshnswamy@devbootcamp.com>
2016-03-04 14:27:06 +1300

    rendering hbs files ok

--- app.js
+++ app.js
@@ -8,8 +8,31 @@ var app = express();
 app.set('pages', path.join(__dirname, 'pages'));
 app.set('view engine', 'hbs');
 
- app.use(bodyParser.json());
- app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+var geoTag = {
+address:[
+{lat: 89.0000},
+{lang: -132.00}
+]
+}
+
+
+app.get('/', function (req, res){
+res.redirect ('index')
+})
+
+app.get('/index', function (req, res){
+
+res.render ('index', geoTag)
+})
+
+app.get('/submit', function (req, res){
+  //console.log(geoTag)
+res.render ('submit')
+})
+
 
 module.exports = app;
