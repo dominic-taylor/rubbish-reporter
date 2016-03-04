c66f836cbb8545f978bf73032edb11d5ed15bf4d
Daniel de la Cruz & lou <pair+danieldelacruz01.louise-r-blue@devbootcamp.com>
2016-03-04 15:03:37 +1300

    separate js and css into seperate files

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
-var GoogleMapsLoader = require('google-maps');
-var dotenv = require('dotenv')
+var map
+var markers = []
+var latitude
+var longitude
+var address
 
-//load env variables
-dotenv.load()
-
-var map = new google.maps.Map();
-
-GoogleMapsLoader.KEY = process.env.MAPS_API_KEY,
-GoogleMapsLoader.LANGUAGE = 'en',
-GoogleMapsLoader.REGION = 'NZ'
-
-GoogleMapsLoader.load(function(google){
-  new google.maps.Map(mapDiv, {
+function initMap() {
+  var mapDiv = document.getElementById('map');
+  var geocoder = new google.maps.Geocoder
+  map = new google.maps.Map(mapDiv, {
     center: {lat: -41.2894828, lng: 174.7759777},
-      zoom: 15
+    zoom: 15
+  });
+  map.addListener('click', function(event){
+    deleteMarkers()
+    placeMarker(event.latLng, map)
+    console.log(event.latLng.lat(), event.latLng.lng())
+    latitude = event.latLng.lat()
+    longitude = event.latLng.lng()
+    geocodeLatLng(geocoder, map)
   })
-})
+}
 
+function placeMarker(latLng, map){
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: map
+  })
+  markers.push(marker)
+}
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
 
-// function initMap(){
-//   var mapDiv = document.getElementById('map')
-//   var map = new google.maps.Map(mapDiv. {
-//     center: {lat: -41.2894828, lng: 174.7759777},
-//       zoom: 15
-//   })
-// }
+function deleteMarkers() {
+  setMapOnAll(null);
+  markers = [];
+}
+
+function geocodeLatLng(geocoder, map){
+  var latlng = {lat: latitude, lng: longitude}
+  geocoder.geocode( {'location': latlng}, function (results, status) {
+    console.log(results[0].formatted_address)
+    address = results[0].formatted_address
+  })
+}
